296d0d30951a322bc3cf6927296b5d8f6f8a2865
pcampina <pablo.dsn@gmail.com>
2021-03-13 12:10:57 -0300

    Trabalhando com componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { Counter } from './components/Counter';
+import { RepositoryList } from './components/RepositoryList';
+
 import './styles/global.scss';
 
 export function App() {
-  return <h1>Hello Folksss!</h1>
+  return (
+    <>
+      <RepositoryList />
+      <Counter />
+    </>
+  )
 }
